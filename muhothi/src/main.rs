26816009ee13dca0fda894d240d3d09134dbc3f4
SitrusMint <chocomnt242@gmail.com>
2020-09-07 07:06:26 +0900

    不必要な部分の削除

--- muhothi/src/main.rs
+++ muhothi/src/main.rs
@@ -2,18 +2,6 @@ mod lib;
 // use lib::{execute_bf};
 
 fn main() {
-    //break文が使える。
-    let mut x = 7;
-    loop {
-        x -= 1;
-        println!("{}", x);
-        if x == 3 { break; }
-    }
-   
-    //continue文が使える。
-    for y in 0..10 {
-        if y % 2 == 0 { continue;/* 飛ばすという意味 */ }
-        
-        println!("{}", y);
-    }
+    // TODO
 }
+
